90510a231308f97542170d1c5df0d727320ad698
Paul Jablonski <pauljablonski@me.com>
2017-05-29 15:59:49 +0100

    Initial sharedPreferences code added.

--- app/src/main/java/com/codeclan/example/todolist/ListOptionActivity.java
+++ app/src/main/java/com/codeclan/example/todolist/ListOptionActivity.java
@@ -1,47 +1,63 @@
 package com.codeclan.example.todolist;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class ListOptionActivity extends AppCompatActivity {
 
-    private TextView toDoQty;
-    private TextView doneQty;
+    private SharedPreferences prefs;
+    private Gson gson = new Gson();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_option);
 
-        toDoQty = (TextView) findViewById(R.id.to_do_num_of_items);
-        doneQty = (TextView) findViewById(R.id.done_num_of_items);
+        TextView toDoQty = (TextView) findViewById(R.id.to_do_num_of_items);
+        TextView doneQty = (TextView) findViewById(R.id.done_num_of_items);
 
-        ToDoList toDoList = new ToDoList();
+        String toDoJson = getToDoListFromSharedPrefs();
+        ToDoList toDoList = getToDoListFromJson(toDoJson);
 
         toDoQty.setText(toDoList.getToDoList().size() + " Items");
         doneQty.setText(toDoList.getDoneList().size() + " Items");
-
     }
 
     public void onToDoListClicked(View button){
-
         Intent intent = new Intent(this, ToDoListActivity.class);
         intent.putExtra("list", "toDoList");
         startActivity(intent);
     }
 
     public void onDoneListClicked(View button){
-
         Intent intent = new Intent(this, ToDoListActivity.class);
         intent.putExtra("list", "doneList");
         startActivity(intent);
+    }
+
+    private String getToDoListFromSharedPrefs(){
+        String sharedPrefsKey = getString(R.string.pref_key);
+        prefs = getSharedPreferences(sharedPrefsKey, Context.MODE_PRIVATE);
+        return prefs.getString("toDoList", gson.toJson(new ToDoList()));
+    }
+
+    private ToDoList getToDoListFromJson(String json){
+        TypeToken<ToDoList> toDoListObject = new TypeToken<ToDoList>(){};
+        return gson.fromJson(json, toDoListObject.getType());
     }
 
 
---
+++ app/src/main/java/com/codeclan/example/todolist/SharedPrefsHelper.java
@@ -0,0 +1,31 @@
+package com.codeclan.example.todolist;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+
+/**
+ * Created by PaulJablonski on 29/05/2017.
+ */
+
+public class SharedPrefsHelper {
+//
+//    private SharedPreferences prefs;
+//    private Gson gson = new Gson();
+//
+//    public static String getToDoListFromSharedPrefs(prefs){
+//        return prefs.getString("toDoList", gson.toJson(new ToDoList()));
+//    }
+//
+//    public static ToDoList getToDoListFromJson(String json){
+//        TypeToken<ToDoList> toDoListObject = new TypeToken<ToDoList>(){};
+//        return gson.fromJson(json, toDoListObject.getType());
+//    }
+
+}
